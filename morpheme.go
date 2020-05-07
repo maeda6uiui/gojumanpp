80bbaa57ba3f0fbad20f35db1c94aa71720b928f
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-05-06 18:40:44 -0700

    Morphemeから使用されていない変数を削除

--- morpheme.go
+++ morpheme.go
@@ -1,24 +1,22 @@
 package gojumanpp
 
-type Morpheme struct{
-	Midasi string
-	Yomi string
-	Genkei string
-	Hinsi string
-	Hinsi_id int
-	Bunrui string
-	Bunrui_id int
-	Katuyou1 string
+type Morpheme struct {
+	Midasi      string
+	Yomi        string
+	Genkei      string
+	Hinsi       string
+	Hinsi_id    int
+	Bunrui      string
+	Bunrui_id   int
+	Katuyou1    string
 	Katuyou1_id int
-	Katuyou2 string
+	Katuyou2    string
 	Katuyou2_id int
-	Imis string
-	Fstring string
-	Repname string
+	Imis        string
+	Repname     string
 }
 
-func NewMorpheme() *Morpheme{
-	m:=new(Morpheme)
+func NewMorpheme() *Morpheme {
+	m := new(Morpheme)
 	return m
 }
-
